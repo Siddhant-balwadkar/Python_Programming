4a3c037a869ca0f7b381128b7221a29d2ed18a83
Siddhant-balwadkar <siddhantvbalwadkar@gmail.com>
2026-06-30 15:04:27 +0530

    Add files via upload

---
+++ TypeDemo.py
@@ -0,0 +1,22 @@
+# Number
+no = 11                 # int
+marks = 90.78           # float
+value = 8+5j            # Complex
+
+print(value)
+
+print(type(no))
+print(type(marks))
+print(type(value))
+
+# Boolean
+Gender = True
+
+print(Gender)
+print(type(Gender))
+
+# None
+Demo = None
+
+print(type(Demo))
+print(Demo)
